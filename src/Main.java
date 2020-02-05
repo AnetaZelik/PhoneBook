90aca7998e755beb7eb554a76b4361d7eb3f3c83
AnetaZelik <aneta.zelik@yahoo.com>
2020-02-05 18:40:14 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+        public static void main(String[] args) {
+            PhoneBook addressBook = new PhoneBook();
+
+            addressBook.add("FirstName", 100000000);
+            addressBook.add("SecondName", 225555550);
+            addressBook.add("ThirdName", 355226630);
+
+            System.out.println(addressBook.checkIfNameExists("SecondName"));
+            System.out.println(addressBook.getNumber("ThirdName"));
+            addressBook.printAll();
+        }
+    }
+
---
+++ src/PhoneBook.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneBook {
+        private Map<String, Integer> phoneBook = new HashMap<>();
+
+        public void add(String name, int number) {
+            phoneBook.put(name, number);
+        }
+
+        public boolean checkIfNameExists(String name) {
+            return phoneBook.containsKey(name);
+        }
+
+        public Integer getNumber(String name) {
+            return phoneBook.get(name);
+        }
+
+        public Integer getSize() {
+            return phoneBook.size();
+        }
+
+        public void printAll() {
+            for (Map.Entry<String, Integer> mapEntry : phoneBook.entrySet()) {
+                System.out.println(mapEntry.getKey()
+                        + "'s number is " + mapEntry.getValue());
+            }
+        }
+
+    }
+
