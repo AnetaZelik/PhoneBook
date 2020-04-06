c33b945f39c275e26e95f15e61ace7671580e965
AnetaZelik <aneta.zelik@yahoo.com>
2020-04-06 13:22:14 +0200

    init

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -2,30 +2,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneBook {
-        private Map<String, Integer> phoneBook = new HashMap<>();
+    private Map<String, Integer> phoneBook = new HashMap<>();
 
-        public void add(String name, int number) {
-            phoneBook.put(name, number);
-        }
-
-        public boolean checkIfNameExists(String name) {
-            return phoneBook.containsKey(name);
-        }
-
-        public Integer getNumber(String name) {
-            return phoneBook.get(name);
-        }
-
-        public Integer getSize() {
-            return phoneBook.size();
-        }
-
-        public void printAll() {
-            for (Map.Entry<String, Integer> mapEntry : phoneBook.entrySet()) {
-                System.out.println(mapEntry.getKey()
-                        + "'s number is " + mapEntry.getValue());
-            }
-        }
-
+    public void add(String name, int number) {
+        phoneBook.put(name, number);
     }
+
+    public boolean checkIfNameExists(String name) {
+        return phoneBook.containsKey(name);
+    }
+
+    public Integer getNumber(String name) {
+        return phoneBook.get(name);
+    }
+
+    public Integer getSize() {
+        return phoneBook.size();
+    }
+
+    public void printAll() {
+        for (Map.Entry<String, Integer> mapEntry : phoneBook.entrySet()) {
+            System.out.println(mapEntry.getKey()
+                    + "'s number is " + mapEntry.getValue());
+        }
+    }
+
+}
 
